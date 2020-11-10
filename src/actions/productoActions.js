e71ba995d9589ffac782687cb8d0567c35db4e3e
jplozada <juxnpx@gmail.com>
2020-11-10 12:30:47 -0400

    Saving Productos GET request data on the State

--- src/actions/productoActions.js
+++ src/actions/productoActions.js
@@ -62,10 +62,27 @@ const agregarProductoError = estado => ({
 export function obtenerProductosAction() {
     return async (dispatch) => {
         dispatch(descargarProductos());
+
+        try {
+            const respuesta = await clienteAxios.get('/productos');
+            dispatch(descargaProductosExitosa(respuesta.data))
+        } catch (error) {
+            dispatch(descargaProductosError())
+        }
     }
 }
 
 const descargarProductos = () => ({
     type: COMENZAR_DESCARGA_PRODUCTOS,
     payload: true 
+})
+
+const descargaProductosExitosa = productos => ({
+    type: DESCARGA_PRODUCTOS_EXITO,
+    payload: productos
+})
+
+const descargaProductosError = () => ({
+    type: DESCARGA_PRODUCTOS_ERROR,
+    payload: true
 })
