20b7f79a188c6d3624792ff42c2a58be0c7640cf
jplozada <juxnpx@gmail.com>
2020-11-10 17:55:35 -0400

    ProductoActions config for Productos DELETE request

--- src/components/Producto.js
+++ src/components/Producto.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+// Redux
+import {useDispatch} from 'react-redux';
+import {borrarProductoAction} from '../actions/productoActions';
 
 const Producto = ({producto}) => {
     
     const {nombre, precio, id} = producto;
+    const dispatch = useDispatch();
+
+    // Confirmar si desea eliminarlo
+    const confirmarEliminarProducto = id => {
+        // Preguntar al usuario
+
+        //Pasarlo al action
+        dispatch(borrarProductoAction(id));
+    }
     
     return (
         <tr>
@@ -14,6 +26,7 @@ const Producto = ({producto}) => {
                 <button
                     type="button"
                     className="btn btn-danger"
+                    onClick={() => confirmarEliminarProducto(id)}
                 >Eliminar</button>
             </td>
         </tr>
