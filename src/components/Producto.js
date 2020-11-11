ce10c49326809a8346bbefab8a841975ea620720
jplozada <juxnpx@gmail.com>
2020-11-11 11:18:02 -0400

    ProductoActions config to update Producto, saving selected Producto on the State

--- src/components/Producto.js
+++ src/components/Producto.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import {Link, useHistory} from 'react-router-dom';
 import Swal from 'sweetalert2';
 // Redux
 import {useDispatch} from 'react-redux';
-import {borrarProductoAction} from '../actions/productoActions';
+import {borrarProductoAction, obtenerProductoEditar} from '../actions/productoActions';
 
 const Producto = ({producto}) => {
     
     const {nombre, precio, id} = producto;
     const dispatch = useDispatch();
+    const history = useHistory(); // Habilitar history para redireccion
 
     // Confirmar si desea eliminarlo
     const confirmarEliminarProducto = id => {
@@ -29,13 +30,24 @@ const Producto = ({producto}) => {
             }
         });
     }
+
+    // Funcion que redirige de forma programada
+    const redireccionarEdicion = producto => {
+        dispatch(obtenerProductoEditar(producto));
+        history.push(`/productos/editar/${producto.id}`);
+    }
     
     return (
         <tr>
             <td>{nombre}</td>
             <td><span className="font-weight-bold">$ {precio}</span></td>
             <td className="acciones">
-                <Link to={`/productos/editar/${id}`} className="btn btn-primary mr-2">Editar</Link>
+                <button 
+                    type="button"
+                    onClick={() => redireccionarEdicion(producto)}
+                    className="btn btn-primary mr-2">
+                    Editar
+                </button>
                 <button
                     type="button"
                     className="btn btn-danger"
